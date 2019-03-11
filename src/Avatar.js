504c8ced3f3d7d928f083e07e84ab0feafc27581
hunter <hunterkeng@hotmail.com>
2019-03-12 08:27:11 +1300

    rate limitition exceed, finish later

--- src/Avatar.js
+++ src/Avatar.js
@@ -3,58 +3,81 @@ import React from "react";
 class Avatar extends React.Component {
     constructor() {
         super();
-        this.state = { searchText: "mojombo",
-        name: "mojombo",
-        followers: 100, 
-        isLoaded:false, 
-        users: [],
-        profileURL: "https://avatars0.githubusercontent.com/u/1?v=4"
-     };
+        this.baseURL = "https://api.github.com/users";
+        this.state = {
+            isLoaded1: false,
+            isLoaded2: false,
+            users: [],
+            user: {
+                "name": "Tom Preston-Werner",
+                "avatar_url": "https://avatars0.githubusercontent.com/u/1?v=4",
+                "followers": 21375
+            }
+        };
         this.update = this.update.bind(this);
+        this.getFullName = this.getFullName.bind(this);
     }
 
     componentDidMount() {
-        fetch("https://api.github.com/users")
+        fetch(this.baseURL)
             .then(res => res.json())
             .then(
                 (result) => {
                     this.setState({
-                        isLoaded: true,
+                        isLoaded1: true,
                         users: result
                     })
                 },
-                (error)=>({
-                    isLoaded: false,
+                (error) => ({
+                    isLoaded1: false,
                     error
                 })
             )
-
-            // console.log(this.state.users);
-
     }
 
     update(e) {
-        console.log(e.target.value);
+        const searchText = e.target.value;
+        console.log(searchText);
         console.log(this.state.users);
-        this.state.users.map((user, index) =>{
-            if(user.login.indexOf(e.target.value > 0)){
-                this.setState({name: user.login, profileURL: user.avatar_url});
+        this.state.users.map((eachUser, index) => {
+            if (eachUser.login.indexOf(e.target.value) > 0) {
+                // this.setState({ user: eachUser });
+                const name = eachUser.login;
+                this.getFullName(name);
             }
             return 0;
-
         });
 
+    }
+
+    getFullName(name) {
+        console.log(name);
+
+        fetch(this.baseURL + "/" + name)
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    this.setState({
+                        user: result,
+                        isLoaded2: true
+                    })
+                },
+                (error) => ({
+                    isLoaded2: false,
+                    error
+                })
+            )
     }
 
     render() {
         return (
             <div>
                 <input type="search" placeholder="search ..." onChange={this.update}></input>
-                <br/><br/>
-                <img src={this.state.profileURL} height="100" width="100" alt="profile"></img>
+                <br /><br />
+                <img src={this.state.user.avatar_url} height="100" width="100" alt="profile"></img>
 
-                <h1>Full Name: {this.state.name}</h1>
-                <h1>Followers: {this.state.followers}</h1>
+                <h1>Full Name: {this.state.user.name}</h1>
+                <h1>Followers: {this.state.user.followers}</h1>
 
             </div>
         );
